07ba526509ec3c9d65e377177095819a5da54ba9
Duvan Felipe Gomez Delgado <161946502+FelipeGomezzz@users.noreply.github.com>
2024-07-31 10:19:41 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    function crearBingo(colorearPrimeraColumna = false, colorearX = false) {
+    function crearBingo(colorearPrimeraColumna = false, colorearX = false, columnaAColorear = 0) {
         let iteracionFilas = 0;
         let iteracionColumnas = 0;
         let total = [];
@@ -42,8 +42,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 const esDiagonalSecundaria = iteracionFilas + iteracionColumnas === 4;
                 
                 // Determinar la clase de la celda
-                let clase = "";
-                if (colorearPrimeraColumna && iteracionColumnas === 0) {
+                let clase = "boton";
+                if (colorearPrimeraColumna && iteracionColumnas === columnaAColorear) {
                     clase += " highlight-columna";
                 } else if (colorearX && (esDiagonalPrincipal || esDiagonalSecundaria)) {
                     clase += " highlight-x";
@@ -59,14 +59,22 @@ document.addEventListener("DOMContentLoaded", function() {
         return text;
     }
 
-    // Crear los dos Bingos
+    // Crear los Bingos
     let bingo1 = crearBingo(false, true); // Primer bingo con la forma de X coloreada en amarillo
     let bingo2 = crearBingo(true); // Segundo bingo con la primera columna en amarillo
+    let bingo3 = crearBingo(true, false, 1); // Tercer bingo con la segunda columna coloreada en amarillo
+    let bingo4 = crearBingo(true, false, 2); // Tercer bingo con la segunda columna coloreada en amarillo
+    let bingo5 = crearBingo(true, false, 3); // Tercer bingo con la segunda columna coloreada en amarillo
+    let bingo6 = crearBingo(true, false, 4); // Tercer bingo con la segunda columna coloreada en amarillo
     
-    // Mostrar ambos Bingos uno al lado del otro
+    // Mostrar los Bingos uno al lado del otro
     let contenedor = document.getElementById("contenido");
     contenedor.innerHTML = "<div class='bingo-container'>" +
                            "<div class='bingo'>" + bingo1 + "</div>" +
                            "<div class='bingo'>" + bingo2 + "</div>" +
+                           "<div class='bingo'>" + bingo3 + "</div>" +
+                           "<div class='bingo'>" + bingo4 + "</div>" +           
+                           "<div class='bingo'>" + bingo5 + "</div>" +
+                           "<div class='bingo'>" + bingo6 + "</div>" +
                            "</div>";
 });
